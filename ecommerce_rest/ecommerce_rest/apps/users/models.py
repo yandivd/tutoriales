7042cb9ffd24d903559b27cfa9fc5961ac22a9c9
Yandi <yandivd@gmail.com>
2022-09-12 11:46:11 -0400

    trabajando en el modelo users

---
+++ ecommerce_rest/ecommerce_rest/apps/users/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from simple_history.models import HistoricalRecords
+
+# Create your models here.
