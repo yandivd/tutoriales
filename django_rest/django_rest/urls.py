d580351a272051c961f2dd889a8c3223e97fb506
Yandi <yandivd@gmail.com>
2022-09-12 21:28:07 -0400

    #

--- django_rest/django_rest/urls.py
+++ django_rest/django_rest/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api1/', include('usuario.api.urls')),
+    path('', include('usuario.api.urls')),
 ]
--- django_rest/usuario/api/urls.py
+++ django_rest/usuario/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from usuario.api.api import UsuarioAPIView
 
 urlpatterns = [
-    path('', UsuarioAPIView.as_view(), name='usuario_api'),
+    path('api1/', UsuarioAPIView.as_view(), name='usuario_api'),
 ]
