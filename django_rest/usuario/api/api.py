b515929ef2aa1a9a1ac5f344983a0a4f6d7a7e09
Yandi <yandivd@gmail.com>
2022-09-13 22:50:00 -0400

    decoradores

--- django_rest/usuario/api/api.py
+++ django_rest/usuario/api/api.py
@@ -1,11 +1,22 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from usuario.models import Usuario
 from usuario.api.serializer import UsuarioSerializer
 
-class UsuarioAPIView(APIView):
-    def get(self, request):
+# class UsuarioAPIView(APIView):
+#     def get(self, request):
+#         usuarios = Usuario.objects.all()
+#         usuarios_serializer = UsuarioSerializer(usuarios, many=True)
+        
+#         return Response(usuarios_serializer.data)
+
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    if request.method == 'GET':
         usuarios = Usuario.objects.all()
         usuarios_serializer = UsuarioSerializer(usuarios, many=True)
         
         return Response(usuarios_serializer.data)
+    elif request.method == 'POST':
+        print(request.data)
--- django_rest/usuario/api/urls.py
+++ django_rest/usuario/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from usuario.api.api import UsuarioAPIView
+# from usuario.api.api import UsuarioAPIView
+from usuario.api.api import user_api_view
 
 urlpatterns = [
-    path('usuarios/', UsuarioAPIView.as_view(), name='usuario_api'),
+    # path('usuarios/', UsuarioAPIView.as_view(), name='usuario_api'),
+    path('usuarios2/', user_api_view, name='usuario_api_view'),
 ]
